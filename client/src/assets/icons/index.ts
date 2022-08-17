0106bd0ea70936c6260170aa35df175ec9c1bc54
Woochul Lee <leewoo0686@gmail.com>
2022-08-17 19:42:56 +0900

    [Feature] 아이콘 추가 및 색상 변수 설정 (#9)
    * feat: 색상 변수 및 텍스트 변수 설정
    
    * :sparkles:feat: 아이콘 파일 추가
    
    * :gear: PR 템플릿 수정
    
    * :hammer:refactor: 색상 변수 객체로 변경 및 텍스트 변수 삭제

---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,16 @@
+export { ReactComponent as Add } from './add.svg';
+export { ReactComponent as Category } from './category.svg';
+export { ReactComponent as Check } from './check.svg';
+export { ReactComponent as Chevron } from './chevron-down.svg';
+export { ReactComponent as ChevronLeft } from './chevron-left.svg';
+export { ReactComponent as ChevronRight } from './chevron-right.svg';
+export { ReactComponent as Close } from './close.svg';
+export { ReactComponent as Heart } from './heart.svg';
+export { ReactComponent as Image } from './image.svg';
+export { ReactComponent as LogOut } from './log-out.svg';
+export { ReactComponent as MapPin } from './map-pin.svg';
+export { ReactComponent as Menu } from './menu.svg';
+export { ReactComponent as Message } from './message-square.svg';
+export { ReactComponent as More } from './more-vertical.svg';
+export { ReactComponent as Send } from './send.svg';
+export { ReactComponent as User } from './user.svg';
---
+++ client/src/constants/colors.ts
@@ -0,0 +1,14 @@
+const color = {
+  titleActive: '#222222',
+  grey1: '#888888',
+  grey2: '#bbbbbb',
+  grey3: '#d7d7d7',
+  offWhite: '#f6f6f6',
+  white: '#ffffff',
+  primary1: '#2ac1bc',
+  primary2: '#a0e1e0',
+  primary3: '#219a95',
+  error: '#f45452',
+};
+
+export default color;
